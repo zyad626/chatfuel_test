45adcba6fd084b264e9b71aad6c5b7f42a2a590a
Zeyad Yasser <zeyad.yasser@travolic.com>
2022-07-31 14:35:30 +0200

    using vanilla js

--- app.js
+++ app.js
@@ -3,14 +3,13 @@ import express from "express"
 const app = express()
 dotenv.config()
 
-const token = "mELtlMAHYqR0BvgEiMq8zVek3uYUK3OJMbtyrdNPTrQB9ndV0fM7lWTFZbM4MZvD";
-const botId = "5ed0e9eb28f2cd636b334007";
+const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res)=>{
 
-    let elements: element[] = []
+    let elements = []
     for(let i = 0; i < 3; i++){
-        let e:element = {
+        let e = {
             title: `test title`,
             image_url:  "https://chatbot-tickets.s3.amazonaws.com/4507365a-dff0-487e-8528-7a6d46e14ab9!0",
             subtitle: `origin - dest`,
@@ -26,7 +25,7 @@ app.get('/', (req, res)=>{
         }
         elements.push(e)
     }
-    const Gallery: gallery = {
+    const Gallery = {
         attachment: {
             type: "template",
             payload: {
@@ -39,28 +38,4 @@ app.get('/', (req, res)=>{
     res.send({ messages: [Gallery] })
 
 })
-app.listen(5000, ()=>console.log("listening on port 5000"))
-type element = {
-    title: string,
-    image_url: string,
-    subtitle: string,
-    default_action: {
-        type: string,
-        url: string
-    },
-    buttons: [{
-        type: string,
-        url: string,
-        title: string
-    }]
-}
-type gallery = {
-    attachment: {
-        type: string,
-        payload: {
-            template_type: string,
-            image_aspect_ratio: string,
-            elements: element[],
-        }
-    }
-}
+app.listen(PORT, ()=>console.log("http://localhost:5000"))
